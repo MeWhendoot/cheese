4deaeb30116e27517dcab143ce24822f42df227b
MeWhendoot <55993997+MeWhendoot@users.noreply.github.com>
2025-01-23 10:51:06 +0000

    Add files via upload

--- cheese.py
+++ cheese.py
@@ -3,13 +3,20 @@ from time import sleep
 i = "x"
 filenum = 0
 
-while i == "x":
-    f = open("cheese" + str(filenum) + ".txt", "w")
-    f.write("cheese")
-    f.close()
+print("Please ensure this file is in a folder without any files already named cheese.")
+start = input("Do you wish to commence the cheese? y/n\n")
+
+if start == "y":
+    while i == "x":
+        f = open("cheese" + str(filenum) + ".txt", "w")
+        f.write("cheese")
+        f.close()
     
-    int(filenum)
-    filenum = filenum + 1
+        int(filenum)
+        filenum = filenum + 1
     
-    #sleep(0.5)
-    continue
+        #sleep(0.5)
+        continue
+    
+else:
+    exit()
